ecb6f47e07cae6b077e5ab048504a0788ff71f33
jinshu2604 <58423617+jinshu2604@users.noreply.github.com>
2019-12-03 13:23:45 +0800

    Update diy.sh
    Repair timecloud bugs

--- diy.sh
+++ diy.sh
@@ -6,4 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.100/g' package/base-files/files/bin/config_generate
+# Repair timecloud bugs
+sed -i '/DTS := Timecloud/a\  IMAGE_SIZE := $(ralink_default_fw_size_16M)' target/linux/ramips/image/mt7621.mk
